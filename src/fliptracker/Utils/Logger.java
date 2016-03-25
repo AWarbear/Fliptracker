58044bb85528035c044e9300b85444825953fb33
AWarbear <runescapepeng@gmail.com>
2016-03-25 22:44:47 +0200

    Clean up and document Logger

--- src/fliptracker/Utils/Logger.java
+++ src/fliptracker/Utils/Logger.java
@@ -6,11 +6,23 @@ package fliptracker.Utils;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Handles console logging, mainly for development as normal users can't see the console
+ */
 public class Logger {
+
+    /**
+     * Print a log message
+     * @param message log message
+     */
     public static void Log(String message) {
         System.out.println(Logger.getTime() + message);
     }
 
+    /**
+     * Fetch the current time
+     * @return current time "[HH:mm:ss] - "
+     */
     private static String getTime() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
         Date date = new Date();
