e08bae7a0c84fc5349b83aca5e03ee47f220581b
AWarbear <runescapepeng@gmail.com>
2016-03-26 13:10:33 +0200

    Clean up and Document AddDialog

--- src/fliptracker/UIComponents/AddDialog.java
+++ src/fliptracker/UIComponents/AddDialog.java
@@ -1,32 +1,36 @@
-/*
- * Decompiled with CFR 0_102.
- * 
- * Could not load the following classes:
- *  javafx.scene.Scene
- *  javafx.stage.Stage
- */
 package fliptracker.UIComponents;
 
 import fliptracker.UIComponents.Controllers.AddDialogController;
 import fliptracker.UIComponents.Controllers.GuiController;
-import fliptracker.UIComponents.ItemPanel;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class AddDialog
-extends Stage {
+public class AddDialog extends Stage {
+    /**
+     * Create the add dialog for adding a new item
+     * @param scene the scene
+     * @param controller guiController
+     * @param control the controller of this
+     */
     public AddDialog(Scene scene, GuiController controller, AddDialogController control) {
-        this.setScene(scene);
-        this.setTitle("Add item");
+        setScene(scene);
+        setTitle("Add item");
         control.setValues(controller, this);
-        this.show();
+        show();
     }
 
+    /**
+     * Create an add dialog for editing an item
+     * @param scene the scene
+     * @param controller guiController
+     * @param control the controller of this
+     * @param panel the item panel to edit
+     */
     public AddDialog(Scene scene, GuiController controller, AddDialogController control, ItemPanel panel) {
-        this.setScene(scene);
-        this.setTitle("Edit item");
+        setScene(scene);
+        setTitle("Edit item");
         control.setValues(controller, this, panel);
-        this.show();
+        show();
     }
 }
 
