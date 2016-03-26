a54ccc1034c49a0261e2e42879f00026f519f546
AWarbear <runescapepeng@gmail.com>
2016-03-26 14:41:59 +0200

    Clean up and Document ItemPanel

--- src/fliptracker/UIComponents/ItemPanel.java
+++ src/fliptracker/UIComponents/ItemPanel.java
@@ -1,20 +1,3 @@
-/*
- * Decompiled with CFR 0_102.
- * 
- * Could not load the following classes:
- *  javafx.collections.ObservableList
- *  javafx.event.ActionEvent
- *  javafx.event.Event
- *  javafx.event.EventHandler
- *  javafx.scene.Node
- *  javafx.scene.control.Button
- *  javafx.scene.control.Label
- *  javafx.scene.control.ListView
- *  javafx.scene.input.MouseEvent
- *  javafx.scene.layout.ColumnConstraints
- *  javafx.scene.layout.GridPane
- *  javafx.scene.layout.Priority
- */
 package fliptracker.UIComponents;
 
 import fliptracker.UIComponents.Controllers.GuiController;
@@ -28,27 +11,47 @@ import javafx.scene.layout.Priority;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
-public class ItemPanel
-extends GridPane {
-    public String itemName;
-    public String type;
-    public int price;
-    public int amount;
-    public int duration;
-    public int timeAfter;
+/**
+ * ItemPanel that holds the item data for a flip
+ */
+public class ItemPanel extends GridPane {
+    
     private final Label nameLabel = new Label("Null");
     private final Label buyLabel = new Label("buy");
     private final Label priceLabel = new Label("0000");
     private final Label amountLabel = new Label("0000");
     private final Label timeLabel = new Label("00:00:00 00.00");
     private final Label cooldownLabel = new Label("On Cooldown!");
-    public final Label durationLabel = new Label("0 Mins");
+    
     public final Button complete = new Button("Complete");
+    
     private final DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
-    private long date;
-    private boolean onCooldown = true;
+    
     private final GuiController controller;
 
+    public final Label durationLabel = new Label("0 Mins");
+    
+    public String itemName;
+    public String type;
+    
+    public int price;
+    public int amount;
+    public int duration;
+    public int timeAfter;
+    
+    private long date;
+    
+    private boolean onCooldown = true;
+    
+    /**
+     * Create the item panel
+     * @param name item name
+     * @param price item price
+     * @param amount amount of the item
+     * @param type flip type (buy/sell)
+     * @param date date
+     * @param controller guiController
+     */
     public ItemPanel(String name, int price, int amount, String type, long date, GuiController controller) {
         this.itemName = name;
         this.price = price;
@@ -104,33 +107,61 @@ extends GridPane {
         this.getChildren().addAll(this.nameLabel, this.buyLabel, this.amountLabel, this.priceLabel, this.complete, this.timeLabel, this.cooldownLabel, edit, this.durationLabel);
     }
 
-    public void update() {
-        this.priceLabel.setText("Price:" + this.price);
-        this.nameLabel.setText("Item: " + this.itemName);
-        this.buyLabel.setText("  " + this.type);
-        this.amountLabel.setText("Amount:" + this.amount);
-        this.timeLabel.setText(this.timeFormat.format(this.date));
-        this.cooldownLabel.setText("");
+    /**
+     * Update the UI data
+     */
+    void update() {
+        priceLabel.setText("Price:" + price);
+        nameLabel.setText("Item: " + itemName);
+        buyLabel.setText("  " + type);
+        amountLabel.setText("Amount:" + amount);
+        timeLabel.setText(timeFormat.format(date));
+        cooldownLabel.setText("");
     }
 
+    /**
+     * Set the date
+     * @param date the date
+     */
     public void setDate(long date) {
         this.date = date;
-        this.timeLabel.setText(this.timeFormat.format(date));
+        timeLabel.setText(timeFormat.format(date));
     }
 
+    /**
+     * Fetch the date
+     * @return the date
+     */
     public long getTime() {
-        return this.date;
+        return date;
     }
 
+    /**
+     * Wether the item is on cooldown
+     * @return is on cooldown
+     */
     public boolean isOnCooldown() {
-        return this.onCooldown;
+        return onCooldown;
     }
 
+    /**
+     * Set wether this is on cooldown
+     * @param cooldown onCooldown
+     */
     public void setOnCooldown(boolean cooldown) {
-        this.cooldownLabel.setText(cooldown ? "On cooldown!" : "Not on cooldown");
-        this.onCooldown = cooldown;
+        cooldownLabel.setText(cooldown ? "On cooldown!" : "Not on cooldown");
+        onCooldown = cooldown;
     }
 
+    /**
+     * Edit the item
+     * @param name new name
+     * @param price new price
+     * @param amount new amount
+     * @param type new type
+     * @param date new date
+     * @param timeAfter the time thats passed since the offer completed
+     */
     public void edit(String name, int price, int amount, String type, long date, int timeAfter) {
         this.itemName = name;
         this.price = price;
@@ -147,26 +178,36 @@ extends GridPane {
         this.controller.updateProfits();
     }
 
+    /**
+     * Setlog, make this itemPanel into a log itemPanel, apply the cooldown etc
+     */
     public void setLog() {
-        this.durationLabel.setText("" + this.getDuration() + " mins");
-        this.complete.setText("Remove");
-        this.complete.setOnAction(e -> {
-            this.controller.logItems.getItems().remove(this);
-            this.controller.profileManager.saveLogFile();
+        durationLabel.setText(getDuration() + " mins");
+        complete.setText("Remove");
+        complete.setOnAction(e -> {
+            controller.logItems.getItems().remove(this);
+            controller.profileManager.saveLogFile();
         }
         );
-        if (this.type.equals("Buy")) {
-            this.cooldownLabel.setText("On cooldown!");
-        }
-        this.controller.updateProfits();
+        if (type.equals("Buy"))
+            cooldownLabel.setText("On cooldown!");
+        controller.updateProfits();
     }
 
+    /**
+     * Fetch the type of this flip,
+     * @return flipType buy/sell
+     */
     public String getType() {
-        return this.type;
+        return type;
     }
 
+    /**
+     * Get the duration of this flip
+     * @return time after starting this offer
+     */
     public int getDuration() {
-        return this.duration;
+        return duration;
     }
 }
 
